2f568e04bc3dd2e1d1c766197a8a50bd8699783d
John David Martin <johndavidfive@gmail.com>
2016-12-23 15:12:14 -0700

    adding multi track demo

---
+++ demo/multi_track_layers/multi_track_layers.js
@@ -0,0 +1,108 @@
+
+var startDemo;
+
+(function () {
+
+    startDemo = function () {
+
+        var singleHiddenNetwork = new NeuralNetworkLayout({});
+
+        var inputLayer = singleHiddenNetwork.createLayer({
+            nodes: 4,
+            id: 'input'
+        });
+
+        var track1_staging = singleHiddenNetwork.createLayer({
+            nodes: 4,
+            id: 'track1_staging'
+        });
+
+        var track1_hidden1 = singleHiddenNetwork.createLayer({
+            nodes: 4,
+            id: 'track1_hidden_1'
+        });
+
+        var track1_hidden1Relu = singleHiddenNetwork.createLayer({
+            nodes: 4,
+            id: 'track1_hidden_1_relu'
+        });
+
+        var track1_hidden2 = singleHiddenNetwork.createLayer({
+            nodes: 4,
+            id: 'track1_hidden_2'
+        });
+
+        var track1_hidden2Relu = singleHiddenNetwork.createLayer({
+            nodes: 4,
+            id: 'track1_hidden_2_relu'
+        });
+
+
+        var track2_staging = singleHiddenNetwork.createLayer({
+            nodes: 4,
+            id: 'track2_staging'
+        });
+
+
+        var softmax = singleHiddenNetwork.createLayer({
+            nodes: 2,
+            id: 'softmax'
+        });
+
+        var output = singleHiddenNetwork.createLayer({
+            nodes: 2,
+            id: 'output'
+        });
+
+
+        /*============ TRACK ONE ============*/
+
+
+
+        inputLayer.connectToLayer(track1_staging, {
+            type: 'direct'
+        });
+
+        track1_staging.connectToLayer(track1_hidden1, {
+            type: 'fullyConnected'
+        });
+
+        track1_hidden1.connectToLayer(track1_hidden1Relu, {
+            type: 'direct'
+        });
+
+        track1_hidden1Relu.connectToLayer(track1_hidden2, {
+            type: 'fullyConnected'
+        });
+
+        track1_hidden2.connectToLayer(track1_hidden2Relu, {
+            type: 'direct'
+        });
+
+        track1_hidden2Relu.connectToLayer(softmax, {
+            type: 'fullyConnected'
+        });
+
+
+        /*============ TRACK TWO ============*/
+
+        inputLayer.connectToLayer(track2_staging, {
+            type: 'direct'
+        });
+
+        track2_staging.connectToLayer(softmax, {
+            type: 'fullyConnected'
+        });
+
+
+        /*============ OUTPUT ============*/
+
+        softmax.connectToLayer(output, {
+            type: 'direct'
+        });
+
+
+        singleHiddenNetwork.render('#container');
+    };
+
+})();
